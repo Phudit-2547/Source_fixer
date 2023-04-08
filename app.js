37fa49169ab107411d27ef9004777c4f51a30696
Phudit Pumcharern <88961123+Phudit-2547@users.noreply.github.com>
2023-04-08 12:29:16 +0700

    Update app.js
    Powered by ChatGPT

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const btn = document.querySelector(".btn")
-const link = document.querySelector(".link")
+const btn = document.querySelector(".btn");
+const link = document.querySelector(".link");
 
-function fixer(str) {
-  str = str.replace(/.*(https:\/\/)/, "https://"); // remove any text before https://
-  if (!str.match(/^https:\/\//)) { // if input don't have https: add it
-    str = 'https://' + str;
+function fixURL(str) {
+  let output = "";
+  let regex = /(https?:\/\/)([\w\-.]+)(\/[\w\-.\/]*)/g; // Regular expression to match URLs
+  let match = regex.exec(str); // Find the first match in the input string
+  
+  if (match !== null) { // If a match is found, fix the URL
+    let domain = match[2];
+    domain = domain.replace("(", "").replace(")", ""); // Remove parentheses, if any
+    output = str.replace(match[2], domain);
   }
-  return str.replace(/\s/g, '').replace(/[()]/g, '');
+  
+  return output;
 }
 
 btn.addEventListener("click", (e) => {
-    let myUrl = fixer(link.value);
-    link.value = myUrl;
-    link.select()
-    link.setSelectionRange(0, 999999)
-    navigator.clipboard.writeText(myUrl)
-    btn.textContent = "Copied!!"
-    setTimeout(() => {
-        btn.textContent = "Copy"
-    }, 3000)
-})
+  let myUrl = fixURL(link.value);
+  link.value = myUrl;
+  link.select();
+  link.setSelectionRange(0, 999999);
+  navigator.clipboard.writeText(myUrl);
+  btn.textContent = "Copied!!";
+  setTimeout(() => {
+    btn.textContent = "Copy";
+  }, 3000);
+});
