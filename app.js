30a000c1e15b8b1f23763fde306b5ff7d5807926
Phudit Pumcharern <88961123+Phudit-2547@users.noreply.github.com>
2023-04-08 13:40:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const btn = document.querySelector(".btn");
 const link = document.querySelector(".link");
 
 function fixer(str) {
+  str = str.replace(/\n/g, ''); // remove any newline characters
+  str = str.replace(/[()]/g, ''); // remove any parentheses
+  str = str.replace(/(https:\/\/www\.)\//, '$1'); // replace any forward slashes after https://www with just a dot
+  str = str.replace(/\/\./g, '/'); // replace any slashes before dots with just a slash
+  str = str.replace(/(\.\/)/g, '.'); // replace any dot-slash sequences with just a dot
+  
   var regex = /(https?:\/\/)?([\w.-]+)(\/[\w.-\/]*)?/i;
   var matches = str.match(regex);
   if (!matches) {
