c1c4f443879ea10c2ed0945ca60f0cd526b102b9
Phudit Pumcharern <88961123+Phudit-2547@users.noreply.github.com>
2023-04-08 12:49:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const btn = document.querySelector(".btn");
 const link = document.querySelector(".link");
 
 function fixer(str) {
-  str = str.replace(/.*(https:\/\/)/, "https://"); // remove any text before https://
+  str = str.replace(/.*?(https:\/\/)/, "https://"); // remove any text before https://
   if (!str.match(/^https:\/\//)) { // if input don't have https: add it
     str = 'https://' + str;
   }
   str = str.replace(/\s/g, ''); // remove any whitespace
   str = str.replace(/[()]/g, ''); // remove any parentheses
   str = str.replace(/\/\./g, '/'); // replace any slashes before dots with just a slash
-  str = str.replace(/\.\/+/g, ''); // remove any dot-slash sequences
-  str = str.replace(/\/+/g, '/'); // remove any double slashes
-  str = str.replace(/https:\/\/(www\.)?\//, "https://www."); // replace first occurrence of https://www./ with https://www/
+  str = str.replace(/(\.\/)/g, '.'); // replace any dot-slash sequences with just a dot
+  
+  if (str.match(/^(https:\/\/)?(www\.)?pixiv\.net/)) {
+    // format Pixiv link
+    str = str.replace(/(https:\/\/)?(www\.)?pixiv\.net\/.*\/(\d+)/, 'https://www.pixiv.net/en/artworks/$3');
+  } else if (str.match(/^(https:\/\/)?(www\.)?twitter\.com/)) {
+    // format Twitter link
+    str = str.replace(/(https:\/\/)?(www\.)?twitter\.com\/.*?\/status\/(\d+)/, 'https://twitter.com/$1/status/$2');
+  }
   return str;
 }
 
