2a687857fac8859db6c56f905103fda96383a074
Phudit Pumcharern <88961123+Phudit-2547@users.noreply.github.com>
2023-05-24 21:13:38 +0700

    Revert change
    Because ChatGPT Make it worse

--- app.js
+++ app.js
@@ -1,47 +1,24 @@
-const btn = document.querySelector(".btn");
-const link = document.querySelector(".link");
+const btn = document.querySelector(".btn")
+const link = document.querySelector(".link")
 
 function fixer(str) {
-   // remove any text before "https://www."
-  str = str.replace(/.*?(https?:\/\/www\..*)/gi, '$1');
-  str = str.replace(/\n/g, ''); // remove any newline characters
-  str = str.replace(/[()]/g, ''); // remove any parentheses
-  str = str.replace(/(https:\/\/www\.)\//g, '$1.'); // replace all forward slashes after https://www with a dot
-  str = str.replace(/\/\./g, '/'); // replace any slashes before dots with just a slash
-  str = str.replace(/(\.\/)/g, '.'); // replace any dot-slash sequences with just a dot
-  
-  var regex = /(https?:\/\/)?([\w.-]+)(\/[\w.-\/]*)?/i;
-  var matches = str.match(regex);
-  if (!matches) {
-    return null;
+  if (!str.match(/^[a-zA-Z]+:///)) {
+  if (str.charAt(0).toLowerCase() === "x") {
+    str = str.substring(1);
   }
-  var protocol = matches[1] || 'https://';
-  var domain = matches[2];
-  var path = matches[3] || '';
-  path = path.replace(/\s/g, '').replace(/[()]/g, '');
-  path = path.replace(/(\.\/)/g, '.');
-
-  if (domain === 'www.pixiv.net') {
-    // format Pixiv link
-    path = path.replace(/^\/?(en\/artworks\/\d+)/, '/en/artworks/$1');
-    domain = 'www.pixiv.net';
-  } else if (domain === 'www.twitter.com') {
-    // format Twitter link
-    path = path.replace(/^\/?(.*?)\/status\/(\d+)/, '/$1/status/$2');
-    domain = 'www.twitter.com';
+  if (!str.match(/^[a-zA-Z]+:\/\//)) {
+    str = 'https://' + str;
   }
-
-  return protocol + domain + path;
+  return str.replace(/\s/g, '').replace(/[()]/g, '');
 }
-
 btn.addEventListener("click", (e) => {
-  let myUrl = fixer(link.value);
-  link.value = myUrl;
-  link.select();
-  link.setSelectionRange(0, 999999);
-  navigator.clipboard.writeText(myUrl);
-  btn.textContent = "Copied!!";
-  setTimeout(() => {
-    btn.textContent = "Copy";
-  }, 3000);
-});
+    let myUrl = fixer(link.value);
+    link.value = myUrl;
+    link.select()
+    link.setSelectionRange(0, 999999)
+    navigator.clipboard.writeText(myUrl)
+    btn.textContent = "Copied!!"
+    setTimeout(() => {
+        btn.textContent = "Copy"
+    }, 3000)
+})
